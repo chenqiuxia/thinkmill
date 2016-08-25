dfda5019f6e9c2748da6dd78b94e578a0b7a1a61
qiuxia.chen <qiuxia.chen@monstar-lab.com>
2016-08-25 17:17:53 +0800

    animation support

--- js/promo.js
+++ js/promo.js
@@ -1,14 +1,14 @@
 /**
  * Created by christy on 16/8/19.
  */
-$(window).scroll(function () {
-    var $animated = $('.nav');
-    if ($animated.length && $animated.offset().top > 700) {
-        $animated.addClass('.navbar-fixed-top');
-    } else {
-        $animated.removeClass('.navbar-fixed-top');/*截止到这里是用来给nav在滑动的时候判断出现和不出现的条件的*/
-    }
-});
+// $(window).scroll(function () {
+//     var $animated = $('.nav');
+//     if ($animated.length && $animated.offset().top > 700) {
+//         $animated.addClass('.navbar-fixed-top');
+//     } else {
+//         $animated.removeClass('.navbar-fixed-top');/*截止到这里是用来给nav在滑动的时候判断出现和不出现的条件的*/
+//     }
+// });
 
 
 $(document).ready(function () {
@@ -21,32 +21,22 @@ $(document).ready(function () {
                 $(this).animate({"opacity":"1"},1000)
             }
 
-        })
-    })
-})
+        });
+        var $nav =$('.nav');
 
-
-$(document).ready(function () {
-    $(window).scroll(function () {
-        $(".help").each(function () {
-            var bottom_of_object=$(this).offset().top+$(this).outerHeight();
-            var bottom_of_window=$(window).height()+$(window).scrollTop();
-            if(bottom_of_window>=bottom_of_object){
-                $(this).animate({"opacity":"1"},1000)
+        $nav.each(function () {
+            var top_of_nav=$(this).offset().top;//$(this).offset().top-$(window).scrollTop();
+            if (top_of_nav>=700){
+                $nav.addClass('show-nav');
+            }else{
+                $nav.removeClass('show-nav');
             }
-
         })
     })
 })
-$(document).ready(function () {
-    $(window).scroll(function () {
-        $(".img-con3").each(function () {
-            var bottom_of_object=$(this).offset().top+$(this).outerHeight();
-            var bottom_of_window=$(window).height()+$(window).scrollTop();
-            if(bottom_of_window>=bottom_of_object){
-                $(this).animate({"opacity":"1"},1000)
-            }
 
-        })
-    })
-})
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//
+//     })
+// })
